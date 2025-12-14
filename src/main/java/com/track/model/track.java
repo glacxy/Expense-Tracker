3b50cf2839ad9ebb207d3a02c05e690c0c6fb05b
glacxy <glaxcygy@gmail.com>
2025-12-14 21:29:37 +0530

    Initilal commit: Added pom.xml and model - EXPENSETRACKER

--- src/main/java/com/track/model/track.java
+++ src/main/java/com/track/model/track.java
@@ -1,4 +1,62 @@
+package com.track.model;
+
+import java.time.LocalDate;
 
 public class track {
+        private LocalDate date;
+        private   double income;
+        private double expense;
+        private String description;
+        private double balance;
+
+    public track(LocalDate date, double income, double expense, String description, double balance) {
+        this.date = date;
+        this.income = income;
+        this.expense = expense;
+        this.description = description;
+        this.balance = balance;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public double getExpense() {
+        return expense;
+    }
+
+    public void setExpense(double expense) {
+        this.expense = expense;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
    
+
+
 }
